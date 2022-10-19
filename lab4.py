760a483c36beaae7b481bd93c441f8152d574099
Bartek <michasbartek@gmail.com>
2022-10-19 22:22:07 +0200

    lab4 done

--- lab4.py
+++ lab4.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class Burglary:
     def __init__(self, pi, pa, pb, pai, pbi) -> None:
         self.dog_alarm = None
@@ -34,42 +35,145 @@ class Burglary:
             return (1 - self.p_a_i) / (1 - self.p_a) * ((self.p_b_i * self.p_i) / self.p_b)
 
 
+def plot_results(x_lst, y_lst, title, y_axis):
+    plt.title(title)
+    plt.xlabel('X RANGE')
+    plt.ylabel(y_axis)
+    plt.plot(x_lst, y_lst)
+    plt.show()
+
+
 burglary = Burglary(0.002, 0.01, 0.5, 0.8, 0.98)
 print(f'Reason from multiple evidence, burglary:')
 print('=========================================')
 burglary.update_silent_alarm(True)
 burglary.update_dog_alarm(True)
-print(f'Silent Alarm: True, Dog: True, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: True, Dog: True, Probability: {burglary.probability():.2%}')
 burglary.update_silent_alarm(True)
 burglary.update_dog_alarm(False)
-print(f'Silent Alarm: True, Dog: False, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: True, Dog: False, Probability: {burglary.probability():.2%}')
 burglary.update_silent_alarm(True)
 burglary.update_dog_alarm(None)
-print(f'Silent Alarm: True, Dog: None, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: True, Dog: None, Probability: {burglary.probability():.2%}')
 burglary.update_silent_alarm(None)
 burglary.update_dog_alarm(True)
-print(f'Silent Alarm: None, Dog: True, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: None, Dog: True, Probability: {burglary.probability():.2%}')
 burglary.update_silent_alarm(False)
 burglary.update_dog_alarm(True)
-print(f'Silent Alarm: False, Dog: True, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: False, Dog: True, Probability: {burglary.probability():.2%}')
 
 burglary = Burglary(0.002, 0.01, 0.5, 0.98, 0.70)
 print(f'Old dog, Good Alarm:')
 print('=========================================')
 burglary.update_silent_alarm(True)
 burglary.update_dog_alarm(True)
-print(f'Silent Alarm: True, Dog: True, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: True, Dog: True, Probability: {burglary.probability():.2%}')
 burglary.update_silent_alarm(True)
 burglary.update_dog_alarm(False)
-print(f'Silent Alarm: True, Dog: False, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: True, Dog: False, Probability: {burglary.probability():.2%}')
 burglary.update_silent_alarm(True)
 burglary.update_dog_alarm(None)
-print(f'Silent Alarm: True, Dog: None, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: True, Dog: None, Probability: {burglary.probability():.2%}')
 burglary.update_silent_alarm(None)
 burglary.update_dog_alarm(True)
-print(f'Silent Alarm: None, Dog: True, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: None, Dog: True, Probability: {burglary.probability():.2%}')
 burglary.update_silent_alarm(False)
 burglary.update_dog_alarm(True)
-print(f'Silent Alarm: False, Dog: True, Probability: {burglary.probability():.2%}')
+print(
+    f'Silent Alarm: False, Dog: True, Probability: {burglary.probability():.2%}')
 
 
+# change in dog
+x = np.linspace(0, 1, 100)
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, 0.98, i)
+    burglary.update_silent_alarm(True)
+    burglary.update_dog_alarm(True)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: True, DOG: False', 'dog sens')
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, 0.98, i)
+    burglary.update_silent_alarm(True)
+    burglary.update_dog_alarm(False)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: True, DOG: None', 'dog sens')
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, 0.98, i)
+    burglary.update_silent_alarm(True)
+    burglary.update_dog_alarm(None)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: None, DOG: True', 'dog sens')
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, 0.98, i)
+    burglary.update_silent_alarm(None)
+    burglary.update_dog_alarm(True)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: False, DOG: True', 'dog sens')
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, 0.98, i)
+    burglary.update_silent_alarm(False)
+    burglary.update_dog_alarm(True)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: True, DOG: True', 'dog sens')
+
+# change in alarm
+x = np.linspace(0, 1, 100)
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, i, 0.98)
+    burglary.update_silent_alarm(True)
+    burglary.update_dog_alarm(True)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: True, DOG: False', 'alarm sens')
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, i, 0.98)
+    burglary.update_silent_alarm(True)
+    burglary.update_dog_alarm(False)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: True, DOG: None', 'alarm sens')
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, i, 0.98)
+    burglary.update_silent_alarm(True)
+    burglary.update_dog_alarm(None)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: None, DOG: True', 'alarm sens')
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, i, 0.98)
+    burglary.update_silent_alarm(None)
+    burglary.update_dog_alarm(True)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: False, DOG: True', 'alarm sens')
+probability_lst = []
+for i in x:
+    burglary = Burglary(0.002, 0.01, 0.5, i, 0.98)
+    burglary.update_silent_alarm(False)
+    burglary.update_dog_alarm(True)
+    probability_lst.append(burglary.probability())
+
+plot_results(x, probability_lst, 'Alarm: True, DOG: True', 'alarm sens')
